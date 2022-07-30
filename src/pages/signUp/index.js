ee1ba2be5a6ed4868cf810d68965d257382e6a5c
Анатолий Мойстренко <61918010+moistrenko@users.noreply.github.com>
2022-07-30 11:46:06 +0500

    Update index.js

--- src/pages/signUp/index.js
+++ src/pages/signUp/index.js
@@ -5,13 +5,48 @@ import "./index.sass";
 
 const props = {
 	inputs: [
-		{ inputClass: "Email", inputType: "email", inputName: "email", placeholder: "Email" },
-		{ inputClass: "login", inputType: "text", inputName: "login", placeholder: "Login" },
-		{ inputClass: "Name", inputType: "text", inputName: "first_name", placeholder: "Name" },
-		{ inputClass: "Surname", inputType: "text", inputName: "second_name", placeholder: "Surname" },
-		{ inputClass: "Phone", inputType: "phone", inputName: "phone", placeholder: "Phone" },
-		{ inputClass: "password", inputType: "password", inputName: "password", placeholder: "Password" },
-		{ inputClass: "Confrim Password", inputType: "password", inputName: "password", placeholder: "Confrim Password" },
+		{ 
+			inputClass: "Email",
+			inputType: "email",
+			inputName: "email",
+			placeholder: "Email"
+		},
+		{
+			inputClass: "login",
+			inputType: "text",
+			inputName: "login",
+			placeholder: "Login"
+		},
+		{
+			inputClass: "Name",
+			inputType: "text",
+			inputName: "first_name",
+			placeholder: "Name"
+		},
+		{
+			inputClass: "Surname",
+			inputType: "text",
+			inputName: "second_name",
+			placeholder: "Surname"
+		},
+		{
+			inputClass: "Phone",
+			inputType: "phone", 
+			inputName: "phone",
+			placeholder: "Phone"
+		},
+		{
+			inputClass: "password",
+			inputType: "password",
+			inputName: "password",
+			placeholder: "Password"
+		},
+		{
+			inputClass: "Confrim Password",
+			inputType: "password",
+			inputName: "password",
+			placeholder: "Confrim Password"
+		},
 	],
 };
 
